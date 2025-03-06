a83b0849a9e71f163b1c486a9063af8101f1c2e6
Rana <rana@Ranas-Mac-mini.local>
2025-03-06 21:51:57 +0700

    hide middleware

--- platform/core/base/src/Providers/EventServiceProvider.php
+++ platform/core/base/src/Providers/EventServiceProvider.php
@@ -18,7 +18,6 @@ use Botble\Base\Facades\MetaBox;
 use Botble\Base\Http\Middleware\AdminLocaleMiddleware;
 use Botble\Base\Http\Middleware\CoreMiddleware;
 use Botble\Base\Http\Middleware\DisableInDemoModeMiddleware;
-use Botble\Base\Http\Middleware\EnsureLicenseHasBeenActivated;
 use Botble\Base\Http\Middleware\HttpsProtocolMiddleware;
 use Botble\Base\Http\Middleware\LocaleMiddleware;
 use Botble\Base\Listeners\AdminNotificationListener;
@@ -99,11 +98,11 @@ class EventServiceProvider extends ServiceProvider
             $router->aliasMiddleware('preventDemo', DisableInDemoModeMiddleware::class);
             $router->middlewareGroup('core', [CoreMiddleware::class]);
 
-            $this->app->extend('core.middleware', function ($middleware) {
-                return array_merge($middleware, [
-                    EnsureLicenseHasBeenActivated::class,
-                ]);
-            });
+            // $this->app->extend('core.middleware', function ($middleware) {
+            //     return array_merge($middleware, [
+            //         EnsureLicenseHasBeenActivated::class,
+            //     ]);
+            // });
 
             add_filter(BASE_FILTER_TOP_HEADER_LAYOUT, function ($options) {
                 try {
