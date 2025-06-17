06693e94a422ce3baba1035015b59ad29e223533
Rana-xD <pannreachseyrana@gmail.com>
2025-06-18 00:44:21 +0700

    Hotfix hide product stock

--- platform/themes/shofy/views/ecommerce/includes/product-availability.blade.php
+++ platform/themes/shofy/views/ecommerce/includes/product-availability.blade.php
@@ -1,7 +1,7 @@
 <div class="number-items-available">
     @if ($product->stock_status == 'on_backorder')
         <p class="text-warning fw-medium fs-6">{{ __('Warning: This product is on backorder and may take longer to ship.') }}</p>
-    @else ($product->isOutOfStock())
+    @elseif ($product->isOutOfStock())
         <span class="text-danger">{{ __('Out of stock') }}</span>
     @endif
     <!-- @else
