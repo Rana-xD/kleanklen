1c24b04e52e907991b8c1ca26445a03e24064453
Rana-xD <pannreachseyrana@gmail.com>
2025-06-18 00:53:50 +0700

    Hotfix V4

--- platform/plugins/ecommerce/resources/views/themes/includes/product-availability.blade.php
+++ platform/plugins/ecommerce/resources/views/themes/includes/product-availability.blade.php
@@ -4,27 +4,4 @@
     @else
         <p></p>
     @endif
-    {{-- Commented out section
-        @if (! $productVariation)
-            <span class="text-danger">{{ __('Not available') }}</span>
-        @else
-            @if ($productVariation->isOutOfStock())
-                <span class="text-danger">{{ __('Out of stock') }}</span>
-            @elseif (! $productVariation->with_storehouse_management || $productVariation->quantity < 1)
-                <span class="text-success">{{ __('Available') }}</span>
-            @elseif ($productVariation->quantity)
-                <span class="text-success">
-                    @if (EcommerceHelper::showNumberOfProductsInProductSingle())
-                        @if ($productVariation->quantity !== 1)
-                            {{ __(':number products available', ['number' => $productVariation->quantity]) }}
-                        @else
-                            {{ __(':number product available', ['number' => $productVariation->quantity]) }}
-                        @endif
-                    @else
-                        {{ __('In stock') }}
-                    @endif
-                </span>
-           @endif
-       @endif
-    --}}
 </div>
